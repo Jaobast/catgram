8b0cd56e4e790a176373705db881e4e4698f1361
Jaobast <153824183+Jaobast@users.noreply.github.com>
2024-04-01 18:54:29 +0200

    7 Profile

--- script/arrays.js
+++ script/arrays.js
@@ -1,5 +1,30 @@
 const profiles = [ 
 {
+    name: 'Sheila',
+    name_profil: '@sheilaKFC',
+    pic_profil: 'https://i.pinimg.com/564x/0a/90/5b/0a905b9c9b4615f2d2663d90f5e6a929.jpg',
+    pics: [
+        'https://i.pinimg.com/564x/fc/e0/b5/fce0b50bed720b73729c975a7e4e8b4a.jpg',
+        'https://i.pinimg.com/564x/e2/8a/5e/e28a5e9002a604e302c4d2c8c672e4f1.jpg',
+        'https://i.pinimg.com/564x/3d/91/56/3d915672aa9fc96bf0fb3d71f868d433.jpg',
+        'https://i.pinimg.com/564x/af/c5/48/afc548db94a646f91a2e80a35d7977c4.jpg',
+        'https://i.pinimg.com/564x/1a/6d/ef/1a6def4ac9467dfeceedc0e8dc8b477d.jpg',
+        'https://i.pinimg.com/564x/e5/4f/34/e54f34e91f53415090a82deea2dac797.jpg',
+        'https://i.pinimg.com/564x/0c/33/36/0c3336bbe08137e832e041e58a310bae.jpg',
+        'https://i.pinimg.com/564x/f6/1f/dd/f61fddc1cfb370c34d674b52341da8b2.jpg'
+    ],
+    text: [
+        'Janis tex 01',
+        'Janis tex 02',
+        'Janis tex 03',
+        'Janis tex 04',
+        'Janis tex 05',
+        'Janis tex 06',
+        'Janis tex 07',
+        'Janis tex 08'
+    ]
+},
+{
     name: 'Otavio & Fernando',
     name_profil: '@cat_couple',
     pic_profil: 'https://i.pinimg.com/564x/b1/cd/b6/b1cdb6acfb350595716ff7549875ea3c.jpg',
@@ -22,6 +47,32 @@ const profiles = [
         'couple tex 06',
         'couple tex 07',
         'couple tex 08'
+    ]
+},
+
+{
+    name: 'Sophie',
+    name_profil: '@sooophie',
+    pic_profil: 'https://i.pinimg.com/564x/67/37/b6/6737b63c4c72c30c9177d7f667a5c1d8.jpg',
+    pics: [
+        'https://i.pinimg.com/564x/86/aa/16/86aa168c2765fc21a4497e0833b292a7.jpg',
+        'https://i.pinimg.com/564x/c5/2d/38/c52d384edca61a399622e2ab11f80d87.jpg',
+        'https://i.pinimg.com/564x/30/8a/c0/308ac00b39519d58212382b609319250.jpg',
+        'https://i.pinimg.com/564x/a9/3b/c6/a93bc600a54032df78528c9ff619f4fb.jpg',
+        'https://i.pinimg.com/564x/80/e7/c7/80e7c786e724f310272c17baa099a803.jpg',
+        'https://i.pinimg.com/564x/70/47/16/704716775a57351269a18e492bb9f7b6.jpg',
+        'https://i.pinimg.com/564x/f1/4a/34/f14a344465de41b46ac9367a776f1632.jpg',
+        'https://i.pinimg.com/564x/33/4d/a2/334da2dfc879b4ecc1df6343db65c399.jpg'
+    ],
+    text: [
+        'Janis tex 01',
+        'Janis tex 02',
+        'Janis tex 03',
+        'Janis tex 04',
+        'Janis tex 05',
+        'Janis tex 06',
+        'Janis tex 07',
+        'Janis tex 08'
     ]
 },
 
@@ -101,5 +152,31 @@ const profiles = [
         'Janis tex 07',
         'Janis tex 08'
     ]
-}
+},
+
+{
+    name: 'Ferdinand',
+    name_profil: '@nando',
+    pic_profil: 'https://i.pinimg.com/564x/bf/65/0b/bf650b95f21fca38a925a0b957cf8189.jpg',
+    pics: [
+        'https://i.pinimg.com/564x/62/24/ad/6224ad754afcb8d6ea9036da86771e33.jpg',
+        'https://i.pinimg.com/564x/ac/de/45/acde455d1e6aaa5ea6912bc160f86e6f.jpg',
+        'https://i.pinimg.com/564x/3e/3c/ba/3e3cba480ab312c0d5f0b57487cbcb11.jpg',
+        'https://i.pinimg.com/564x/0c/97/5b/0c975b8865e77be0fa35e3c3a830f873.jpg',
+        'https://i.pinimg.com/564x/59/ac/05/59ac0519aa19efa73d54b27a2cbeb2c2.jpg',
+        'https://i.pinimg.com/564x/95/df/3d/95df3d87d40819db07892b402781a7d7.jpg',
+        'https://i.pinimg.com/564x/61/d2/3a/61d23a59e9a1493883258587f2b51ad0.jpg',
+        'https://i.pinimg.com/564x/2f/7f/3c/2f7f3c0ca0e1fc30d8b261528e044782.jpg'
+    ],
+    text: [
+        'Janis tex 01',
+        'Janis tex 02',
+        'Janis tex 03',
+        'Janis tex 04',
+        'Janis tex 05',
+        'Janis tex 06',
+        'Janis tex 07',
+        'Janis tex 08'
+    ]
+},
 ]
--- script/main.js
+++ script/main.js
@@ -28,7 +28,7 @@ function displayProfile(profile) {
 
 window.addEventListener('load', function() {
     const profileCatBlack = profiles.find(function(profile) {
-        return profile.name_profil === '@big.jan';
+        return profile.name_profil === '@nando';
     });
     if (profileCatBlack) {
         displayProfile(profileCatBlack);
